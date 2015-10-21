ef9add7febf8950d9acb6c0f5f85ace8bc49256f
frebern <frebern@naver.com>
2015-10-21 16:56:49 +0900

    Create cheater.php

---
+++ lab6/cheater.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Grade Store</title>
+		<link href="http://selab.hanyang.ac.kr/courses/cse326/2015/problems/pResources/gradestore.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<?php
+		# Ex 4 : 
+		# Check the existance of each parameter using the PHP function 'isset'.
+		# Check the blankness of an element in $_POST by comparing it to the empty string.
+		# (can also use the element itself as a Boolean test!)
+		# if (){
+			$retry = 0;
+			if(!(isset($_POST["name"]) && isset($_POST["id"]) && isset($_POST["course"]) && isset($_POST["grade"]) && isset($_POST["cardnum"]) && isset($_POST["cardkind"]))){
+				$retry=1;
+			}
+			elseif(is_null($_POST["name"])||is_null($_POST["id"])||is_null($_POST["course"])||is_null($_POST["grade"])||is_null($_POST["cardnum"])||is_null($_POST["cardkind"])){
+				$retry=1;
+			}
+			print($retry);
+			if(retry==1){
+				print($retry);
+		?>
+
+		<!-- Ex 4 : 
+			Display the below error message : -->
+			<h1>Sorry</h1>
+			<p>You didn't fill out the form completely. Try again?</p>
+		<!----> 
+
+		<?php
+		# Ex 5 : 
+		# Check if the name is composed of alphabets, dash(-), ora single white space.
+			} elseif(!preg_match("/^[a-zA-Z]*$/",$name)){ 
+		?>
+
+		<!-- Ex 5 : 
+			Display the below error message : -->
+			<h1>Sorry</h1>
+			<p>You didn't provide a valid name. Try again?</p>
+		<!-- --> 
+
+		<?php
+			}
+		# Ex 5 : 
+		# Check if the credit card number is composed of exactly 16 digits.
+		# Check if the Visa card starts with 4 and MasterCard starts with 5. 
+		# } elseif () {
+		?>
+
+		<!-- Ex 5 : 
+			Display the below error message : 
+			<h1>Sorry</h1>
+			<p>You didn't provide a valid credit card number. Try again?</p>
+		--> 
+
+		<?php
+		# if all the validation and check are passed 
+		# } else {
+		?>
+
+		<h1>Thanks, looser!</h1>
+		<p>Your information has been recorded.</p>
+		
+		<!-- Ex 2: display submitted data -->
+		<?php
+			$name = htmlspecialchars($_POST["name"]);
+			$id = htmlspecialchars($_POST["id"]);
+			$arr_course = $_POST["course"];
+			$grade = htmlspecialchars($_POST["grade"]);
+			$cardnum = htmlspecialchars($_POST["cardnum"]);
+			$cardkind = htmlspecialchars($_POST["cardkind"]);
+			$str_course = htmlspecialchars(processCheckbox($arr_course));
+		?>
+		<ul> 
+			<li>Name: <?=$name?></li>
+			<li>ID: <?=$id?></li>
+			<!-- use the 'processCheckbox' function to display selected courses -->
+			<li>Course: <?=$str_course?></li>
+			<li>Grade: <?=$grade?></li>
+			<li>Credit <?=$cardnum?> (<?=$cardkind?>)</li>
+		</ul>
+		
+		<!-- Ex 3 : 
+			<p>Here are all the loosers who have submitted here:</p> -->
+		<?php
+			$filename = "loosers.txt";
+			/* Ex 3: 
+			 * Save the submitted data to the file 'loosers.txt' in the format of : "name;id;cardnumber;cardtype".
+			 * For example, "Scott Lee;20110115238;4300523877775238;visa"
+			 */
+			$looser = join(";",array($name,$id,$cardnum,$cardkind))."\n";
+			file_put_contents($filename, $looser, FILE_APPEND);
+		?>
+		
+		<!-- Ex 3: Show the complete contents of "loosers.txt".
+			 Place the file contents into an HTML <pre> element to preserve whitespace -->
+
+		<pre><?=file_get_contents($filename)?></pre>
+		
+		<?php
+			/* Ex 2: 
+			 * Assume that the argument to this function is array of names for the checkboxes ("cse326", "cse107", "cse603", "cin870")
+			 * 
+			 * The function checks whether the checkbox is selected or not and 
+			 * collects all the selected checkboxes into a single string with comma seperation.
+			 * For example, "cse326, cse603, cin870"
+			 */
+			function processCheckbox($names){
+				return join(", ",$names);		
+			}
+		?>
+		
+	</body>
+</html>
